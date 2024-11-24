e4a8a3a976b3bb664420dc582f40e068d3f4625f
Aleksey512 <yakunina1111@gmail.com>
2024-11-24 13:11:10 +0300

    style: format code

--- services/gateway/application/api/main.py
+++ services/gateway/application/api/main.py
@@ -1,18 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from gateway.api.lifespan import lifespan
+from gateway.application.api.lifespan import lifespan
 from gateway.settings.logging import DEBUG, setup_logging
 
 
 def create_app() -> FastAPI:
     app = FastAPI(
-        title="AllocUp SSO",
+        title="API Gateway",
         root_path="/api",
         lifespan=lifespan,
     )
-    app.add_middleware(
-        CORSMiddleware,
+    app.add_middleware(  # type: ignore[arg-type]
+        CORSMiddleware,  # type: ignore
         allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
@@ -22,9 +22,9 @@ def create_app() -> FastAPI:
     return app
 
 
-def main():
+if __name__ == "__main__":
     conf = {
-        "app": "api.main:create_app",
+        "app": "gateway.application.api.main:create_app",
         "host": "0.0.0.0",
         "port": 8000,
         "log_config": setup_logging(),
@@ -32,7 +32,3 @@ def main():
         "reload": True,
     }
     uvicorn.run(**conf)
-
-
-if __name__ == "__main__":
-    main()
